f3f294d4184f9f0a9ea1c091c5ec53b80a0f0e20
mikeob <mikael.olangcay-brisebois.1@ens.etsmtl.ca>
2021-12-04 11:45:47 -0500

    initial commit

---
+++ src/main/java/com/mike/project/resumeloader/ResumeLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.mike.project.resumeloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeLoaderApplication.class, args);
+	}
+
+}
