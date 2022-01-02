8423a1f7ef0375295b3ded4da88424e35fbbda41
mikeob <mikael.olangcay-brisebois.1@ens.etsmtl.ca>
2022-01-02 05:51:34 -0500

    added spring security

--- src/main/java/com/mike/project/resumeloader/securingweb/Controller.java
+++ src/main/java/com/mike/project/resumeloader/securingweb/Controller.java
@@ -1,4 +1,4 @@
-package com.mike.project.resumeloader;
+package com.mike.project.resumeloader.securingweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
